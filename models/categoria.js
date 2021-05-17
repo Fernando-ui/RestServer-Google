dd4d1dc107520e766b7b486ddd569ee15925e5ee
fersan <fst-96@hotmail.com>
2021-05-17 14:20:57 -0500

    Añadiendo index y categoria

---
+++ models/categoria.js
@@ -0,0 +1,28 @@
+
+
+const { Schema, model} = require('mongoose');
+
+
+const categoriaChema = Schema({
+
+    nombre:{
+        type:String,
+        required:[true, 'El nombre es obligatorio']
+    },
+    estado:{
+        type:Boolean,
+        default:true,
+        required:true
+    },
+    usuario:{
+        // Esto para que apunte al id correcto, sera otro objeto que tendremos en Mongo
+        type: Schema.Types.ObjectId,
+        // Estamos relacionando este usuario con el schema Usuario
+        // Esta referencia tiene que ser igual al nuestro schema de usuario
+        ref:'Usuario',
+        required:true
+    }
+
+});
+
+module.exports = model('Categoria', categoriaChema);
---
+++ models/index.js
@@ -0,0 +1,14 @@
+
+
+const Categoria = require('./categoria');
+const Role = require('./categoria');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+module.exports = {
+
+    Categoria,
+    Role,
+    Server,
+    Usuario
+}
