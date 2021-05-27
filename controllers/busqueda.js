fe1e9c1036ab23b312e3405c11abbfa850d8e894
fersan <fst-96@hotmail.com>
2021-05-27 15:22:49 -0500

    Añadiendo la busqueda en base de  datos

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -1,23 +1,74 @@
 const { response } = require("express");
+const { ObjectId } = require('mongoose').Types;
+
+const {Usuario, Categoria, Producto } = require('../models')
+
+const coleccionesPermitidas = [
+
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles'
+
+]
+
+const buscarUsuarios = async(termino = '', res = response) =>{
 
 
+    const esMongoID = ObjectId.isValid(termino);
+
+    if(esMongoID){
+
+        const usuario = await Usuario.findById(termino)
+
+        res.json({
+            result: (usuario) ? [usuario] : ['No hay usuario']
+        })
+    }
+
+}
 
 const buscar = (req, res = response ) => {
 
 
     const {coleccion, termino} = req.params;
 
-    res.json({
+    if(!coleccionesPermitidas.includes(coleccion)){
 
-        msg:'Todo ha salido bien guapeton',
-        coleccion,
-        termino
-    })
+        return res.status(400).json({
+            msg:`Las colecciones permitidas son ${coleccionesPermitidas}`
+        })
+    }
 
+    switch (coleccion) {
+        case'usuarios':
+
+            buscarUsuarios(termino, res);
+
+        break;
+
+        case'categoria':
+
+        break;
+
+        case'productos':
+
+        break;
+
+        default:
+
+            res.status(500).json({
+                msg:'Se le olvido hacer una busqueda'
+            })
+
+    }
+
+    
 }
 
 module.exports = {
 
-    buscar
+    buscar,
+    buscarUsuarios
 
 }
